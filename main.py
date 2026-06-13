c7289f47f7e39061d1e15061cd173f9561990c19
TaherT2 <taherone18@gmail.com>
2026-06-13 06:26:46 +0400

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,24 @@ from pydantic import BaseModel
 class EditRequest(BaseModel):
     text: str
 
+from pdf_editor import replace_text_in_pdf
+from fastapi.responses import Response
+from pydantic import BaseModel
+
+class ReplaceRequest(BaseModel):
+    old_text: str
+    new_text: str
+
+@app.post("/replace-text")
+async def replace_text(file: UploadFile = File(...),old_text: str = "",new_text: str = ""):
+
+    pdf_bytes = await file.read()
+
+    edited_pdf = replace_text_in_pdf(pdf_bytes,old_text,new_text)
+
+    return Response(content=edited_pdf,media_type="application/pdf",headers={"Content-Disposition":"attachment;filename=edited.pdf"})
+
+
 @app.post("/test-edit")
 def test_edit(data: EditRequest):
 

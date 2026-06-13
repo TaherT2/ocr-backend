08391ee34bc6f53e1bf2a426bb877623b862074e
TaherT2 <taherone18@gmail.com>
2026-06-14 03:27:07 +0400

    Update pdf_editor.py

--- pdf_editor.py
+++ pdf_editor.py
@@ -1,36 +1,37 @@
 import fitz
 
 
-def export_pdf_from_blocks(pdf_bytes, ocr_result):
-    """
-    Rebuilds PDF using edited OCR JSON blocks.
-    """
+def rebuild_pdf(original_pdf_bytes, ocr_data):
 
-    pdf = fitz.open(stream=pdf_bytes, filetype="pdf")
+    pdf = fitz.open(stream=original_pdf_bytes, filetype="pdf")
 
-    for page_data in ocr_result["pages"]:
-
+    for page_data in ocr_data["pages"]:
         page_index = page_data["page"] - 1
         page = pdf[page_index]
 
-        # Go through all blocks
         for block in page_data["blocks"]:
+
+            if "edited_text" not in block:
+                continue
+
+            new_text = block["edited_text"]
+            old_text = block["text"]
 
             x0, y0, x1, y1 = block["box"]
 
             rect = fitz.Rect(x0, y0, x1, y1)
 
-            # 1. erase old text area
+            # remove old text (mask)
             page.draw_rect(
                 rect,
                 color=(1, 1, 1),
                 fill=(1, 1, 1)
             )
 
-            # 2. write new text
+            # insert new text inside same box
             page.insert_textbox(
                 rect,
-                block["text"],
+                new_text,
                 fontsize=block.get("size", 12),
                 fontname="helv",
                 color=(0, 0, 0),
